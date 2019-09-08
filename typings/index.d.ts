a87caa5a1641802b68ef90827768092b3f0bc7b5
Sergey Kambalin <greyexpert@gmail.com>
2019-09-08 09:54:37 +0600

    Typings refactoring

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,21 +1,13 @@
-import { ComponentType } from 'react';
+import { ComponentType, ComponentProps } from 'react';
 
-type Props = {
-  [key: string]: unknown;
-};
-
-type Attributes = {
-  [key: string]: string;
-};
-
-export type Options = {
+export type Options<P> = {
   tag?: string;
   extends?: string;
   attrs?: string[];
   methods?: string[];
   styles?: string | string[];
-  props?: (attributes: Attributes, element: HTMLElement) => Props;
+  props?: (attributes: { [key: string]: string }, element: HTMLElement) => P;
 };
 
 export function css(css: TemplateStringsArray): string;
-export default function (options?: Options): (Component: ComponentType) => HTMLElement;
+export default function <T extends ComponentType>(options?: Options<ComponentProps<T>>): (Component: T) => HTMLElement;
